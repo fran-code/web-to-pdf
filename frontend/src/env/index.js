136ca97815137228156557b907e1856cbe33354f
Kiko <thekikiyo@gmail.com>
2022-04-19 14:59:44 +0200

    env vars docker

--- frontend/src/env/index.js
+++ frontend/src/env/index.js
@@ -2,6 +2,6 @@ import development from './development'
 import production from './production'
 
 const joinEnv = { development, production }
-const env = joinEnv[process.env.NODE_ENV]
+const env = joinEnv[process.env.REACT_APP_ENV || process.env.NODE_ENV]
 
 export default env
